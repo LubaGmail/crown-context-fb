3413297d553360de5b696bc6923b0bc6416921cf
LL Powe <powe2009@gmail.com>
2024-05-20 17:23:48 -0400

    abc

--- src/contexts/shop.context.jsx
+++ src/contexts/shop.context.jsx
@@ -14,15 +14,24 @@ export const ProductProvider = ({ children }) => {
     const [categoriesMap, setCategoryMap] = useState({})
     const value = { products, setProducts, categoriesMap };
 
+    /**
+        (5) [{…}, {…}, {…}, {…}, {…}]
+            0:   {title: 'Hats', items: Array(9)}
+                                        0: {featured: false, name: 'Brown Brim', price: 25, id: 1, imageUrl: 'https:/...}
+     */
     useEffect(() => {
         const getData = async () => {
             let data = await getCategories()
             setProducts(data)
         }
         getData()
-
     }, [])
 
+    /**
+       {hats: Array(9), jackets: Array(5), mens: Array(6), sneakers: Array(8), womens: Array(7)}
+        hats: 
+                0: {id: 1, imageUrl: 'https://i..png', featured: false, name: 'Brown Brim', price: 25}
+     */
     useEffect(() => {
         let map = products.reduce((acc, el, i) => {
             const { title, items } = el
@@ -31,7 +40,6 @@ export const ProductProvider = ({ children }) => {
             return acc
         }, {}) 
         setCategoryMap(map)
-
     }, [products])
     
     return (
